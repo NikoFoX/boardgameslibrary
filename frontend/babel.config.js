d311acd900122d9599131fca9b1636c5d0265ce4
MrN <nikofoxy@gmail.com>
2020-03-22 23:24:17 +0100

    Eslint vue extends added

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [["transform-imports", {}]]
-};
+	presets: ['@vue/cli-plugin-babel/preset'],
+	plugins: [['transform-imports', {}]]
+}
