d3e10557b76b1da4a14c7f0c6b66616d298b6445
MrN <nikofoxy@gmail.com>
2020-10-08 00:09:52 +0200

    First timeline item stylings

--- frontend/src/views/matches/list/components/MatchItem.vue
+++ frontend/src/views/matches/list/components/MatchItem.vue
@@ -1,20 +1,22 @@
 <template>
-    <v-card elevation="15" :flat="!isGameDetails" :color="isGameDetails ? 'blue-grey lighten-4' : ''">
-      <v-card-title v-if="match.gameTitle" class="py-1 pl-2 pr-2">
-        {{ match.gameTitle.title }}
+    <v-card elevation="15" :color="isGameDetails ? 'blue-grey lighten-4' : ''">
+      <v-card-title v-if="match.gameTitle" class="py-1 pl-2 pr-2 text-break">
+        <span class="h6">{{ match.gameTitle.title }}</span>
       </v-card-title>
       <v-card-text class="text-dark">
-        <v-row>
-        <!-- <span class="h6">{{ match.played.split(' ')[0] }}</span> -->
-        <span class="h6">{{ match.played.split('T')[0] }}</span>
-        <v-chip color="warning" class="ml-auto text-dark" small v-if="match.points">
-          <i class="fas fa-star mr-2"></i>
-          <span class="mr-2">{{ match.points }}</span>
-        </v-chip>
-        </v-row>
+        <div class="d-flex justify-space-between m-n2">
+          <span class="h6">{{ match.played.split('T')[0] }}</span>
+          <v-chip color="warning" class=" text-dark" small v-if="match.points">
+            <i class="fas fa-star mr-2"></i>
+            <span class="mr-2">{{ match.points }}</span>
+          </v-chip>
+        </div>
         <v-row justify-space-between class="align-center">
           <v-col v-if="match.team.length > 0" cols="12" class="py-1">
-            <span>Team: {{ match.team.join(', ') }}</span>
+            <span>
+              <v-chip color="blue" small>Team</v-chip>
+              {{ match.team.join(', ') }}
+            </span>
           </v-col>
           <v-col v-if="match.opponents.length > 0" cols="12" class="text-center pt-0 pb-1">
             <v-chip color="primary" small class="versus-chip">vs</v-chip>
@@ -32,10 +34,13 @@
             </v-chip>
           </v-col>
         </v-row>
+        <v-row v-if="match.scenario">
+          <v-col>
+            <v-chip color="blue" small>Scenario</v-chip>
+            {{ match.scenario }}
+          </v-col>
+        </v-row>
       </v-card-text>
-      <v-card-actions>
-        <span style="font-size: 0.875em" v-if="match.scenario">Scenario: {{ match.scenario }}</span>
-      </v-card-actions>
     </v-card>
 </template>
 
