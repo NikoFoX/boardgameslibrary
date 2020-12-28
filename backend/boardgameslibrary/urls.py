200dd40a8e9f1264df617e4fb3cbf98bc0dc83d5
MrN <nikofoxy@gmail.com>
2020-12-28 15:49:19 +0100

    WIP backend tests

--- backend/boardgameslibrary/urls.py
+++ backend/boardgameslibrary/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/games/', include('games.api.urls'))
+    path('api/games/', include('games.api.urls'), name='api_games')
 ]
