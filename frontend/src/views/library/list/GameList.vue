311a1cb182aeaecfe394533f44c3b981e3a3d6dd
sborowski <sborowski@papu.io>
2021-08-19 23:37:40 +0200

    No games info + lint fix

--- frontend/src/views/library/list/GameList.vue
+++ frontend/src/views/library/list/GameList.vue
@@ -1,33 +1,55 @@
 <template>
-	<v-container fluid grid-list-xs>
-		<v-row>
-			<v-col v-for="game in games" :key="game.id" cols="auto">
-				<v-hover v-slot:default="{ hover }">
-					<v-card
-						class="game-card"
-						@click="gameDetails()"
-						color="transparent"
-						:flat="!!game.image"
-						:to="{ name: 'GameDetails', params: { id: game.id } }"
-						hover
-					>
-						<div v-if="game.image">
-							<v-img
-								:src="game.image"
-								height="200px"
-								max-width="275px"
-								contain
-							></v-img>
-							<v-overlay absolute v-if="hover">
-								<span class="game-title">{{ game.title }}</span>
-							</v-overlay>
-						</div>
-						<span v-else class="game-title">{{ game.title }}</span>
-					</v-card>
-				</v-hover>
-			</v-col>
-		</v-row>
-	</v-container>
+  <v-container
+    fluid
+    grid-list-xs
+  >
+    <v-row>
+      <template v-if="games.length === 0">
+        <v-col>
+          <div class="text-center">
+            No games here yet :(
+          </div>
+        </v-col>
+      </template>
+      <template v-else>
+        <v-col
+          v-for="game in games"
+          :key="game.id"
+          cols="auto"
+        >
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              class="game-card"
+              color="transparent"
+              :flat="!!game.image"
+              :to="{ name: 'GameDetails', params: { id: game.id } }"
+              hover
+              @click="gameDetails()"
+            >
+              <div v-if="game.image">
+                <v-img
+                  :src="game.image"
+                  height="200px"
+                  max-width="275px"
+                  contain
+                />
+                <v-overlay
+                  v-if="hover"
+                  absolute
+                >
+                  <span class="game-title">{{ game.title }}</span>
+                </v-overlay>
+              </div>
+              <span
+                v-else
+                class="game-title"
+              >{{ game.title }}</span>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -39,12 +61,12 @@ export default {
 	computed: {
 		...mapGetters(['games'])
 	},
+  mounted() {
+		this.getGames()
+	},
 	methods: {
 		...mapActions(['getGames'])
 	},
-	mounted() {
-		this.getGames()
-	}
 }
 </script>
 
